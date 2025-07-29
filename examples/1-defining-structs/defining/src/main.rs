6be06f75eb922a015dd1e60496568612b8c1b131
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2025-07-29 13:32:55 -0400

    update to basic struct

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -4,6 +4,8 @@ struct Person {
     first_name: String,
     last_name: String,
     age: u8,
+    email: String,
+    phone_number: String,
 }
 
 fn main() {
@@ -11,5 +13,7 @@ fn main() {
         first_name: "John".to_string(),
         last_name: "Doe".to_string(),
         age: 25,
+        email: "23johnny@gmail.com".to_string(),
+        phone_number: "123-456-7890".to_string(),
     });
 }
