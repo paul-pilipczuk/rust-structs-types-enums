97b1d619715345aa842ae92ffdb08d3e6d367793
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2025-07-29 13:40:11 -0400

    added notes into Person Struct and added in .unwrap to access Option

--- examples/2-creating-structs/creating/src/main.rs
+++ examples/2-creating-structs/creating/src/main.rs
@@ -3,14 +3,14 @@ struct Person {
     first_name: String,
     last_name: String,
     age: Option<u8>,
-}
+}   
 
 fn main() {
     let alfredo = Person{
         first_name: "Alfredo".to_string(),
         last_name: "Sanchez".to_string(),
-        age: Some(23),
+        age: Some(23), //can also add in None, since we have the option enum, but NOT Some(None)
     };
     println!("The person's first name is: {}", alfredo.first_name);
-    println!("The person's age is: {:?}", alfredo.age);
+    println!("The person's age is: {:?}", alfredo.age.unwrap()); //unwrap() is used to get the value inside the Option
 }
