28603750081a61ba7fb1562a22d80fc7b5869b50
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2025-07-30 11:43:41 -0400

    changed filesize to implement format size

--- examples/14-match-enums/match-enum/src/main.rs
+++ examples/14-match-enums/match-enum/src/main.rs
@@ -5,24 +5,25 @@ enum FileSize {
     Gigabytes(f64),
 }
 
-fn format_size(size: u64) -> String {
+impl  FileSize{   
+    fn format_size(&self) -> String {
+        match self {
+            FileSize::Bytes(bytes) => format!("{} bytes", bytes),
+            FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
+            FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
+            FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
+        }
+    }
+}
+
+
+fn main() {
+    let size = 2_000_000_000;
     let filesize = match size {
         0..=999 => FileSize::Bytes(size),
         1000..=999_999 => FileSize::Kilobytes(size as f64 / 1000.0),
         1_000_000..=999_999_999 => FileSize::Megabytes(size as f64 / 1_000_000.0),
         _ => FileSize::Gigabytes(size as f64 / 1_000_000_000.0),
     };
-
-    match filesize {
-        FileSize::Bytes(bytes) => format!("{} bytes", bytes),
-        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb),
-        FileSize::Megabytes(mb) => format!("{:.2} MB", mb),
-        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb),
-    }
-}
-
-
-fn main() {
-    let result = format_size(6888837399);
-    println!("{}", result)
+    println!("{}", filesize.format_size());
 }
